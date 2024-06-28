860756f3542213fd8707c2ca16f890f9db5a9847
kgw10 <aa01059183132@gmail.com>
2024-06-28 17:31:44 +0900

    0628

--- java_basic/src/java0628/java2.java
+++ java_basic/src/java0628/java2.java
@@ -8,35 +8,82 @@ public class java2 {
 	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
+
+		// 음료 검색하여 음료 이름과 금액이 출력되게 하세요
+		// 카페 배열에 없는 음료 검색시 죄송합니다. 준비중입니다. 라고 출력	
+		
+		String[] 카페 = new String[] {
+				"아메리카노 : 2000원", "화이트 아메리카노 : 2800원", "카페라떼 : 3000원", "돌체라떼 : 3800원"
+				, "카페모카 : 3800원", "수박주스 : 4000원", "써머아이스티 : 2900원", "복숭아 아이스티 : 2800원"
+				, "자두 아이스티 : 2800원", "유자 에이드 : 3500원", "레몬 에이드 : 3500원", 
+				"청포도 에이드 : 3500원"
+		};
+		
+		
+		
+		System.out.print("음료를 입력하세요 : ");
+		String keyword = scan.nextLine();
+		
+		boolean isDrink = true;
+		for( int i = 0; i < 카페.length; i++) {
+			if(카페[i].contains(keyword)) {
+				System.out.println(카페[i]);
+				isDrink = false;
+			} 
+			
+		}
+		if(isDrink) {
+			System.out.println("죄송합니다. 준비중입니다.");
+		}
+		
+		
+		
+		□ㅁ
+		
+		
+		
+		
+		
+		
+//		String[] name = new String[] {
+//			"김유신", "한석봉", "양만춘", "김춘추", "정도전", "정약용", "이순신", "이사부", "이사람", "한가인"
+//		};
+//		
+//		System.out.print("이름 검색 : ");
+//		String search = scan.nextLine();
+//		
+//		for( int i = 0; i < name.length; i++) {
+//			if( name[i].contains(search)) {
+//				System.out.println("검색 결과 : " + name[i]);
+//			}
+//		}
+//		
+		
+		
+		
+//		String word = "i like banana";
+//		
+//		// 문자열에서 특정 문자나 문자열의 위치 찾기 - indexOf
+//		System.out.println(word.indexOf("like"));
+//		
+//		// 문자열에서 특정 문자나 문자열의 존재 유무 - true,false
+//		System.out.println(word.contains("bn"));
+//		String[] a = new String[] {"banana", "orange"};
+//		
+//		a[0].indexOf("na"); // 2
+//		a[1].contains("ge"); // true
 		
 		
 		
 		// name 배열의 문자열 중에서 이성계, 문익점, 정도전, 최영을 선발대 배열에 저장하고 출력하세요.
-		String[] name = new String[] {
-				"이순신", "김유신", "장보고", "이성계", "문익점", "정도전", "최영", "양만춘"
-		};
-		
-		String[] 선발대 = new String[4];
-		System.arraycopy( name,3, 선발대, 0, 4);
-		System.out.println(Arrays.toString(선발대));
+//		String[] name = new String[] {
+//				"이순신", "김유신", "장보고", "이성계", "문익점", "정도전", "최영", "양만춘"
+//		};
+//		
+//		String[] 선발대 = new String[4];
+//		System.arraycopy( name,3, 선발대, 0, 4);
+//		System.out.println(Arrays.toString(선발대));
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		
 		
 		
